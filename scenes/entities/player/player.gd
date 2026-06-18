9548f0145a5e3629957b020edf15a37d049b8a8f
rheamin <rheamin11@proton.me>
2026-06-18 13:29:13 -0600

    added attack animation

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -9,6 +9,7 @@ enum State {
 
 @export_category("Stats")
 @export var speed: int = 400
+@export var attack_speed: float = 0.6
 
 var state: State = State.IDLE
 var move_dir: Vector2 = Vector2.ZERO
@@ -16,9 +17,18 @@ var move_dir: Vector2 = Vector2.ZERO
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var animation_playback: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
 
+func _ready() -> void:
+	animation_tree.set_active(true)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		attack()
+
 
 func _physics_process(delta: float) -> void:
-	movement_loop()
+	if not state == State.ATTACK:
+		movement_loop()
 
 
 func movement_loop() -> void:
@@ -55,3 +65,20 @@ func update_animation() -> void:
 			animation_playback.travel("run")
 		State.ATTACK:
 			animation_playback.travel("attack")
+
+
+func attack() -> void:
+	# check if already attacking
+	if state == State.ATTACK:
+		return
+	state = State.ATTACK
+
+	# find attack direction
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	var attack_dir: Vector2 = (mouse_pos - global_position).normalized()
+	$Sprite2D.flip_h = attack_dir.x < 0 and abs(attack_dir.x) >= abs(attack_dir.y)
+	animation_tree.set("parameters/attack/BlendSpace2D/blend_position", attack_dir)
+	update_animation()
+
+	await get_tree().create_timer(attack_speed).timeout
+	state = State.IDLE
